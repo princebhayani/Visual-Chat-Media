a2369225fa333714370197a5eb007f0c58789758
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-17 14:46:08 +0530

    Add Dummy Conversation Data

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -1,9 +1,11 @@
 import { ListFilter, LogOut, MessageSquareDiff, Search, User } from "lucide-react";
 import { Input } from "../ui/input";
 import ThemeSwitch from "./theme-switch";
+import { conversations } from "@/dummy-data/db";
+import Conversation from "./conversation";
 
 const LeftPanel = () => {
-	const conversations = [];
+	
 
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
@@ -38,7 +40,9 @@ const LeftPanel = () => {
 			{/* Chat List */}
 			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
 				{/* Conversations will go here*/}
-
+				{conversations?.map((conversation) => (
+					<Conversation key={conversation._id} conversation={conversation} />
+				))}
 				{conversations?.length === 0 && (
 					<>
 						<p className='text-center text-gray-500 text-sm mt-3'>No conversations yet</p>
