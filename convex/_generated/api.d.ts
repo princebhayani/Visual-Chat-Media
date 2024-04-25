2c78a6ce9e61c58aea0907b07bb8c478748140e6
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-25 13:58:22 +0530

    connect clerk and convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as clerk from "../clerk.js";
+import type * as http from "../http.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,7 +25,10 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  clerk: typeof clerk;
+  http: typeof http;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+	providers: [
+		{
+			domain:process.env.CLERK_ISSUER_URL,
+			applicationID: "convex",
+		},
+	],
+};
+
+export default authConfig;
