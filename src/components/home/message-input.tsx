9ae9c202541f8261766b243b2b18d7243c4c1d97
Prince Bhayani <princebhayani192003@gmail.com>
2024-05-04 15:28:05 +0530

    send text message

--- src/components/home/message-input.tsx
+++ src/components/home/message-input.tsx
@@ -2,10 +2,28 @@ import { Laugh, Mic, Plus, Send } from "lucide-react";
 import { Input } from "../ui/input";
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import toast from "react-hot-toast";
+import { useConversationStore } from "@/store/chat-store";
 
 const MessageInput = () => {
 	const [msgText, setMsgText] = useState("");
+	const { selectedConversation } = useConversationStore();
 
+	const me = useQuery(api.users.getMe);
+	const sendTextMsg = useMutation(api.messages.sendTextMessage);
+
+	const handleSendTextMsg = async (e: React.FormEvent) => {
+		e.preventDefault();
+		try {
+			await sendTextMsg({ content: msgText, conversation: selectedConversation!._id, sender: me!._id });
+			setMsgText("");
+		} catch (err: any) {
+			toast.error(err.message);
+			console.error(err);
+		}
+	};
 	return (
 		<div className='bg-gray-primary p-2 flex gap-4 items-center'>
 			<div className='relative flex gap-2 ml-2'>
@@ -14,7 +32,7 @@ const MessageInput = () => {
 				<Plus className='text-gray-600 dark:text-gray-400' />
 			</div>
 			
-			<form className='w-full flex gap-3'>
+			<form onSubmit={handleSendTextMsg} className='w-full flex gap-3'>
 				<div className='flex-1'>
 					<Input
 						type='text'
