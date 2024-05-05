072d7b05ce6bcfe56e4aa0ca46465dd7d840de05
Prince Bhayani <princebhayani192003@gmail.com>
2024-05-05 18:50:21 +0530

    add get messages query

--- convex/messages.ts
+++ convex/messages.ts
@@ -46,3 +46,49 @@ export const sendTextMessage = mutation({
 		
 	},
 });
+
+
+export const getMessages = query({
+	args: {
+		conversation: v.id("conversations"),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("Unauthorized");
+		}
+
+		const messages = await ctx.db
+			.query("messages")
+			.withIndex("by_conversation", (q) => q.eq("conversation", args.conversation))
+			.collect();
+
+		const userProfileCache = new Map();
+
+		const messagesWithSender = await Promise.all(
+			messages.map(async (message) => {
+				if (message.sender === "ChatGPT") {
+					const image = message.messageType === "text" ? "/gpt.png" : "dall-e.png";
+					return { ...message, sender: { name: "ChatGPT", image } };
+				}
+				let sender;
+				// Check if sender profile is in cache
+				if (userProfileCache.has(message.sender)) {
+					sender = userProfileCache.get(message.sender);
+				} else {
+					// Fetch sender profile from the database
+					sender = await ctx.db
+						.query("users")
+						.filter((q) => q.eq(q.field("_id"), message.sender))
+						.first();
+					// Cache the sender profile
+					userProfileCache.set(message.sender, sender);
+				}
+
+				return { ...message, sender };
+			})
+		);
+
+		return messagesWithSender;
+	},
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -54,3 +54,32 @@ export const isSameDay = (timestamp1: number, timestamp2: number): boolean => {
 	);
 };
 
+export const getRelativeDateTime = (message: any, previousMessage: any) => {
+	const today = new Date();
+	const yesterday = new Date(today);
+	yesterday.setDate(yesterday.getDate() - 1);
+	const lastWeek = new Date(today);
+	lastWeek.setDate(lastWeek.getDate() - 7);
+
+	const messageDate = new Date(message._creationTime);
+
+	if (!previousMessage || !isSameDay(previousMessage._creationTime, messageDate.getTime())) {
+		if (isSameDay(messageDate.getTime(), today.getTime())) {
+			return "Today";
+		} else if (isSameDay(messageDate.getTime(), yesterday.getTime())) {
+			return "Yesterday";
+		} else if (messageDate.getTime() > lastWeek.getTime()) {
+			const options: Intl.DateTimeFormatOptions = {
+				weekday: "long",
+			};
+			return messageDate.toLocaleDateString(undefined, options);
+		} else {
+			const options: Intl.DateTimeFormatOptions = {
+				day: "2-digit",
+				month: "2-digit",
+				year: "numeric",
+			};
+			return messageDate.toLocaleDateString(undefined, options);
+		}
+	}
+};
