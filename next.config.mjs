90377d47dceb2b08c1cd8ee9c72f12238ea06904
Prince Bhayani <princebhayani192003@gmail.com>
2024-05-21 00:28:55 +0530

    dall-e3 picture view bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{ hostname: "stoic-sturgeon-730.convex.cloud" },
+			{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
 		],
 	},
 };
